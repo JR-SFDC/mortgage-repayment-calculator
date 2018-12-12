0c6880f514b9b4ad3176b9ca1c8e4f4b2073b120
Elly Zhu <ezhu0808@gmail.com>
2018-12-12 18:04:01 +1100

    blah

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// blah
 app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }));
 app.use(express.static('public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
